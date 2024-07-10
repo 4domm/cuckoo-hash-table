da46e6f69c59b71dd7206e77ec8f39099677f349
Georgiy Yurchenko <gryurchenko@edu.hse.ru>
2024-07-11 01:44:10 +0300

    jellyfish

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <functional>
+#include <bits/stdc++.h>
 
 template<typename KeyType, typename ValueType, typename Hash1 = std::hash<KeyType>, typename Hash2 = std::hash<KeyType>>
 class CuckooHashTable {
 private:
     size_t table_size_ = 10;  // Initial capacity
     static const int32_t kMaxTries = 7;
-    std::vector<std::pair<std::pair<KeyType, ValueType>, bool>> first_table_;
-    std::vector<std::pair<std::pair<KeyType, ValueType>, bool>> second_table_;
+    std::vector<std::pair<KeyType, ValueType>> first_table_;
+    std::vector<std::pair<KeyType, ValueType>> second_table_;
     int32_t count_elements_;
     Hash1 hash_function1_;
     Hash2 hash_function2_;
@@ -31,24 +28,24 @@ private:
             int32_t hash;
             if (table_index == 1) {
                 hash = HashFunction1(key);
-                if (!first_table_[hash].second) {
-                    first_table_[hash] = {{key, value}, true};
+                if (first_table_[hash].first == KeyType()) {
+                    first_table_[hash] = {key, value};
                     count_elements_++;
                     return;
                 } else {
-                    std::swap(key, first_table_[hash].first.first);
-                    std::swap(value, first_table_[hash].first.second);
+                    std::swap(key, first_table_[hash].first);
+                    std::swap(value, first_table_[hash].second);
                     table_index = 2;
                 }
             } else {
                 hash = HashFunction2(key);
-                if (!second_table_[hash].second) {
-                    second_table_[hash] = {{key, value}, true};
+                if (second_table_[hash].first == KeyType()) {
+                    second_table_[hash] = {key, value};
                     count_elements_++;
                     return;
                 } else {
-                    std::swap(key, second_table_[hash].first.first);
-                    std::swap(value, second_table_[hash].first.second);
+                    std::swap(key, second_table_[hash].first);
+                    std::swap(value, second_table_[hash].second);
                     table_index = 1;
                 }
             }
@@ -62,20 +59,20 @@ private:
         int32_t hash1 = HashFunction1(key);
         int32_t hash2 = HashFunction2(key);
 
-        return (first_table_[hash1].second && first_table_[hash1].first.first == key) ||
-               (second_table_[hash2].second && second_table_[hash2].first.first == key);
+        return (first_table_[hash1].first == key) ||
+               (second_table_[hash2].first == key);
     }
 
     bool Remove(const KeyType &key) {
         int32_t hash1 = HashFunction1(key);
         int32_t hash2 = HashFunction2(key);
 
-        if (first_table_[hash1].second && first_table_[hash1].first.first == key) {
-            first_table_[hash1].second = false;
+        if (first_table_[hash1].first == key) {
+            first_table_[hash1].first = KeyType();
             count_elements_--;
             return true;
-        } else if (second_table_[hash2].second && second_table_[hash2].first.first == key) {
-            second_table_[hash2].second = false;
+        } else if (second_table_[hash2].first == key) {
+            second_table_[hash2].first = KeyType();
             count_elements_--;
             return true;
         }
@@ -88,30 +85,28 @@ private:
         auto old_second_table = std::move(second_table_);
 
         table_size_ *= 2;
-        first_table_ = std::vector<std::pair<std::pair<KeyType, ValueType>, bool>>(table_size_,
-                {{KeyType(), ValueType()}, false});
-        second_table_ = std::vector<std::pair<std::pair<KeyType, ValueType>, bool>>(table_size_,
-                {{KeyType(), ValueType()}, false});
+        first_table_ = std::vector<std::pair<KeyType, ValueType>>(table_size_, {KeyType(), ValueType()});
+        second_table_ = std::vector<std::pair<KeyType, ValueType>>(table_size_, {KeyType(), ValueType()});
 
         count_elements_ = 0;
 
         for (const auto &pair: old_first_table) {
-            if (pair.second) {
-                InsertInternal(pair.first.first, pair.first.second, 1);
+            if (pair.first != KeyType()) {
+                InsertInternal(pair.first, pair.second, 1);
             }
         }
 
         for (const auto &pair: old_second_table) {
-            if (pair.second) {
-                InsertInternal(pair.first.first, pair.first.second, 1);
+            if (pair.first != KeyType()) {
+                InsertInternal(pair.first, pair.second, 1);
             }
         }
     }
 
 public:
     CuckooHashTable(Hash1 hash1 = Hash1(), Hash2 hash2 = Hash2())
-            : first_table_(table_size_, {{KeyType(), ValueType()}, false}),
-              second_table_(table_size_, {{KeyType(), ValueType()}, false}),
+            : first_table_(table_size_, {KeyType(), ValueType()}),
+              second_table_(table_size_, {KeyType(), ValueType()}),
               count_elements_(0),
               hash_function1_(hash1),
               hash_function2_(hash2) {}
@@ -124,12 +119,12 @@ public:
         return count_elements_;
     }
 
-    void SearchKey(const KeyType &key) const {
-        (Search(key));
+    bool SearchKey(const KeyType &key) const {
+        return Search(key);
     }
 
-    void RemoveKey(const KeyType &key) {
-        Remove(key);
+    bool RemoveKey(const KeyType &key) {
+        return Remove(key);
     }
 };
 
